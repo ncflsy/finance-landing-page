4f4a52bd05eb6a1f10f635a1fd1a96ad21d61312
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-07 08:24:03 +0700

    finance landing page slicing

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: '#333333',
+        purple: '#C396E7',
+        grey: '#E7EDF3'
+      }
+    },
+  },
+  plugins: [],
+}
+
